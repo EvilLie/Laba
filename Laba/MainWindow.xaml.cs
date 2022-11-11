ac78f1dea32bb482b4cd0797f2ec17bdfb4952b7
Artemiy Sadriev <mosin1898@mail.ru>
2022-11-11 21:28:26 +0300

    Add project files.

---
+++ Laba/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Laba
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientButton_Click(object sender, RoutedEventArgs e)
+        {
+            Client_Window p = new Client_Window();
+            p.Show();
+            Hide();
+        }
+
+        private void BuhButton_Click(object sender, RoutedEventArgs e)
+        {
+            Buh_Window p = new Buh_Window();
+            p.Show();
+            Hide();
+        }
+        private void ManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager_Window p = new Manager_Window();
+            p.Show();
+            Hide();
+        }
+    }
+}
