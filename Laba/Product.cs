3aa5bbf70040c2856220a4dcf0bbe1b73875e828
Artemiy Sadriev <mosin1898@mail.ru>
2022-11-17 19:31:42 +0300

    Some classes

---
+++ Laba/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Laba
+{
+    internal class Product
+    {
+        public Product(int idProduct, string productName, string productDescription, string productCategory, double productPrice)
+        {
+            IdProduct = idProduct;
+            this.productName = productName;
+            this.productDescription = productDescription;
+            this.productCategory = productCategory;
+            this.productPrice = productPrice;
+        }
+        private int idProduct;
+        private string productName;
+        private string productDescription;
+        private double productPrice;
+        private string productCategory;
+        public int IdProduct { get => idProduct; set => idProduct = value; }
+        public string ProductName { get => productName; set => productName = value; }
+        public string ProductDescription { get => productDescription; set => productDescription = value; }
+        public string ProductCategory { get => productCategory; set => productCategory = value; }
+        public double ProductPrice { get => productPrice; set => productPrice = value; }
+    }
+}
---
+++ Laba/Storage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba
+{
+    internal class Storage
+    {
+        private string adressStorage;
+
+        public Storage(string adressStorage)
+        {
+            this.adressStorage = adressStorage;
+        }
+
+        private int idStorage;
+
+        public Storage(int idStorage)
+        {
+            this.idStorage = idStorage;
+        }
+
+        private List<Product> products;
+
+        public Storage(List<Product> products)
+        {
+            this.products = products;
+        }
+
+        public string AdressStorage { get => adressStorage; set => adressStorage = value; }
+        public int IdStorage { get => idStorage; set => idStorage = value; }
+        internal List<Product> Products { get => products; set => products = value; }
+
+    }
+}
