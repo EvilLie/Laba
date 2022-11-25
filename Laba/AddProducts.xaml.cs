c209272f62b3ea247c30c25971294dfe101f2ce4
Artemiy Sadriev <mosin1898@mail.ru>
2022-11-26 02:25:14 +0300

    Update a little bit better

--- Laba/AddProducts.xaml.cs
+++ Laba/AddProducts.xaml.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
+using System.Windows;
 
 namespace Laba
 {
     public partial class AddProducts : Window
     {
-        private int prodCol ,prodPrice;
+        private int prodCol, prodPrice;
         private string prodName, sql;
         private DB conn = new DB();
         private MySqlCommand command;
         MySqlDataReader reader;
         DataTable dataTable = new DataTable();
- 
+
         public AddProducts()
         {
             InitializeComponent();
@@ -33,16 +22,34 @@ namespace Laba
 
         private void Update_DataTableButton_Click(object sender, RoutedEventArgs e)
         {
-            dtGrid.ItemsSource = null;
-            dtGrid.Items.Clear();   
-            sql = "SELECT * FROM products";
-            dataTable = new DataTable();
-            command = new MySqlCommand(sql, conn.GetConnection());
-            reader = command.ExecuteReader();
-            dataTable.Load(reader);
-            reader.Close();
-            conn.CloseConnection();
-            dtGrid.ItemsSource = dataTable.DefaultView;
+            if (conn.OpenConnection() == true)
+            {
+                dtGrid.ItemsSource = null;
+                dtGrid.Items.Clear();
+                sql = "SELECT * FROM products";
+                dataTable = new DataTable();
+                command = new MySqlCommand(sql, conn.GetConnection());
+                reader = command.ExecuteReader();
+                dataTable.Load(reader);
+                reader.Close();
+                conn.CloseConnection();
+                dtGrid.ItemsSource = dataTable.DefaultView;
+            }
+            else
+            {
+                conn.OpenConnection();
+                dtGrid.ItemsSource = null;
+                dtGrid.Items.Clear();
+                sql = "SELECT * FROM products";
+                dataTable = new DataTable();
+                command = new MySqlCommand(sql, conn.GetConnection());
+                reader = command.ExecuteReader();
+                dataTable.Load(reader);
+                reader.Close();
+                conn.CloseConnection();
+                dtGrid.ItemsSource = dataTable.DefaultView;
+            }
+
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -54,15 +61,15 @@ namespace Laba
 
         private void OpenProductList()
         {
-                conn.OpenConnection();
-                sql = "SELECT * FROM products";
-                command = new MySqlCommand(sql,conn.GetConnection());
-                reader = command.ExecuteReader();
-                dataTable.Load(reader);
-                reader.Close();
-                conn.CloseConnection();
-                dtGrid.ItemsSource = dataTable.DefaultView;
-                 
+            conn.OpenConnection();
+            sql = "SELECT * FROM products";
+            command = new MySqlCommand(sql, conn.GetConnection());
+            reader = command.ExecuteReader();
+            dataTable.Load(reader);
+            reader.Close();
+            conn.CloseConnection();
+            dtGrid.ItemsSource = dataTable.DefaultView;
+
         }
         private void AddProductButton_Click(object sender, RoutedEventArgs e)
         {
@@ -96,11 +103,11 @@ namespace Laba
                     }
                 }
             }
-            catch(Exception)
+            catch (Exception)
             {
                 MessageBoxResult result = MessageBox.Show("There is letter in product number, or product cost please try again");
                 return;
-            }         
-        }     
+            }
+        }
     }
 }
